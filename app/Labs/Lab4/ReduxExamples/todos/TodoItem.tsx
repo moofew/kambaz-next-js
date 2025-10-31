73e4f90a6340b800e4e1d2be7632e426b7cffacf
Matthew Tong <mtong0722@gmail.com>
2025-10-31 14:58:27 -0400

    trying to fix TodoItem error

--- app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -2,7 +2,12 @@ import { Button, ListGroupItem } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 
-export default function TodoItem({todo}) {
+interface Todo {
+  id: string;
+  title: string;
+}
+
+export default function TodoItem({todo}: {todo: Todo}) {
   const dispatch = useDispatch();
   return (
     <ListGroupItem key={todo.id}>
